b8a62b9db74e7373baf54ff996058cb0e98d1c79
Austin Moore <austin@austinsdk.me>
2025-11-14 17:15:34 -0600

    added a bigger smile :)

--- docs/index.js
+++ docs/index.js
@@ -4,4 +4,12 @@ document.addEventListener("DOMContentLoaded",dom=>{
     // document.querySelector("#headerimage").addEventListener("mouseover",e=>{
     //     document.querySelector("#headerimage").classList.remove("selected")
     // })
+
+    // replace small w/ big image
+    let _img = document.querySelector("#altimage");
+    let _bImage = new Image();
+    _bImage.src = "art/big_icon.webp";
+    _bImage.onload = ()=>{
+        _img.style.backgroundImage = `url(${_bImage.src})`;
+    }
 })
