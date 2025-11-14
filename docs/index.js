6e615587246d601907e18d4ee817b3bd98fad236
Austin Moore <133458852+Null-Austin@users.noreply.github.com>
2025-11-14 18:16:46 +0000

    deprecated rehover feature

--- docs/index.js
+++ docs/index.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded",dom=>{
     // force the user to hover and unhover to make the model photo smaller!
-    document.querySelector("#headerimage").addEventListener("mouseover",e=>{
-        document.querySelector("#headerimage").classList.remove("selected")
-    })
+    // deprecated, i think it looks better expanded
+    // document.querySelector("#headerimage").addEventListener("mouseover",e=>{
+    //     document.querySelector("#headerimage").classList.remove("selected")
+    // })
 })
